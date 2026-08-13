6300afd3b3168e2ee714cf6f118b8ec912977fe7
LastLife <129353401+sidwiskers@users.noreply.github.com>
2026-08-13 13:56:12 +0530

    Persist device appearance preference

--- apps/mobile/lib/data/repositories/preferences_repository.dart
+++ apps/mobile/lib/data/repositories/preferences_repository.dart
@@ -1,10 +1,12 @@
 import '../../core/notifications/notification_config.dart';
+import '../../core/theme/nyla_appearance.dart';
 import '../database/app_database.dart';
 
 class PreferencesRepository {
   PreferencesRepository(this.database);
 
   static const _notificationKey = 'notifications.config.v1';
+  static const _appearanceKey = 'appearance.mode.v1';
 
   final AppDatabase database;
 
@@ -16,4 +18,13 @@ class PreferencesRepository {
 
   Future<void> setNotificationConfig(NotificationConfig value) =>
       database.setPreference(_notificationKey, value.encode());
+
+  Stream<NylaAppearance> watchAppearance() =>
+      database.watchPreference(_appearanceKey).map(decodeNylaAppearance);
+
+  Future<NylaAppearance> appearance() async =>
+      decodeNylaAppearance(await database.preference(_appearanceKey));
+
+  Future<void> setAppearance(NylaAppearance value) =>
+      database.setPreference(_appearanceKey, encodeNylaAppearance(value));
 }
